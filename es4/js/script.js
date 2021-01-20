a6dcbe4700ab69405a00ad2e0502a5b32c78d74d
Leandro <thesquallrock@gmail.com>
2021-01-20 16:54:53 +0100

    init es 4

---
+++ es4/js/script.js
@@ -0,0 +1,25 @@
+// Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
+
+const listaBici = [
+  {
+    nome : 'Bici1',
+    peso : 20
+  },
+  {
+    nome : 'Bici2',
+    peso : 12
+  },
+  {
+    nome : 'Bici3',
+    peso : 10
+  },
+  {
+    nome : 'Bici4',
+    peso : 15
+  },
+  {
+    nome : 'Bici5',
+    peso : 18
+  },
+];
